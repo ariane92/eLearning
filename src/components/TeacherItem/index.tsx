19fc5369684272ea386fcaa5f2151f57ce33ad77
Ariane Mateus <ariane.ccomp@gmail.com>
2020-08-21 19:49:58 -0300

    feat: component TeacherList

---
+++ src/components/TeacherItem/index.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import {Image} from 'react-native';
+import {
+  Container,
+  Profile,
+  Avatar,
+  ProfileInfo,
+  Name,
+  Subject,
+  Bio,
+  Footer,
+  Price,
+  PriceValue,
+  FavoriteButton,
+  ContactButton,
+  ContactButtonText,
+  ButtonsContainer,
+} from './styles';
+
+import heartOutlineIcon from '../../assets/images/icons/heart-outline.png';
+import unFavoriteIcon from '../../assets/images/icons/unfavorite.png';
+import whatsAppIcon from '../../assets/images/icons/whatsapp.png';
+
+const TeacherItem = () => {
+  return (
+    <Container>
+      <Profile>
+        <Avatar source={{uri: 'https://github.com/ariane92.png'}} />
+        <ProfileInfo>
+          <Name>Ariane Mateus</Name>
+          <Subject>Prog</Subject>
+        </ProfileInfo>
+      </Profile>
+      <Bio>Entusiasta por conversa de bar, filosofa de boteco</Bio>
+      <Footer>
+        <Price>
+          Preço/hora{'   '}
+          <PriceValue>R$ 20,00</PriceValue>
+        </Price>
+        <ButtonsContainer>
+          <FavoriteButton>
+            <Image source={heartOutlineIcon} />
+          </FavoriteButton>
+          <ContactButton>
+            <Image source={whatsAppIcon} />
+            <ContactButtonText>Entrar em contato</ContactButtonText>
+          </ContactButton>
+        </ButtonsContainer>
+      </Footer>
+    </Container>
+  );
+};
+
+export default TeacherItem;
--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -3,11 +3,13 @@ import {View} from 'react-native';
 
 import {Container} from './styles';
 import PageHeader from '../../components/PageHeader';
+import TeacherItem from '../../components/TeacherItem';
 
 const TeacherList = () => {
   return (
     <Container>
       <PageHeader title="Proffys disponíveis" />
+      <TeacherItem />
     </Container>
   );
 };
